9bd720d505a8d2459db50547b24555e7888f5067
Oleh Krehel <ohwoeowho@gmail.com>
2016-08-11 13:52:58 +0200

    toolbox/emacsdocomplete.m: Fix whitespace

--- toolbox/emacsdocomplete.m
+++ toolbox/emacsdocomplete.m
@@ -4,22 +4,23 @@ function emacsdocomplete(substring)
 % completions.  This hides the differences between versions
 % for the calls needed to do completions.
 
+
     v = ver('MATLAB');
 
-    
+
     if str2double(v.Version) < 8.4
-        
+
         % Pre R2014b: partial_string
         extracmd = '';
-        
-    else        
-        
+
+    else
+
         % Post R2014b: partial_string, caret, num
         extracmd = [ ', ' num2str(length(substring)) ',0' ];
 
         % DEV NOTE: If you find a test failure, contact Eric Ludlam
         % to also update matlab-emacs SF repository.
-        
+
     end
 
     command = [ 'matlabMCRprocess_emacs = com.mathworks.jmi.MatlabMCR;' ...
@@ -30,5 +31,4 @@ function emacsdocomplete(substring)
     % Completion engine needs to run in the base workspace to know
     % what the variables you have to work with are.
     evalin('base',command);
-    
 end
